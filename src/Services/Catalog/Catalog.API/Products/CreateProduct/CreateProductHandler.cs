aaf896294bd9b0dcbeab415c5d6f18d96baf0b67
taleh.karimov <taleh.karimov@smartsolutions.az>
2024-06-17 18:01:58 +0400

    Create request lifecycle with CQRS pattern

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(Guid Id, string Name, List<string> Category, 
+        string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            Product product = MapCommandToProduct(command);
+
+            //TODO: save to db
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+
+        private Product MapCommandToProduct(CreateProductCommand command)
+        {
+            return new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+        }
+    }
+}
