3b611d77e60275d3b85dedc4ca000080f35a4cca
taleh.karimov <taleh.karimov@smartsolutions.az>
2024-07-01 15:44:59 +0400

    Create db and add inital migration with seed data

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddDbContext<DiscountContext>(opts =>
 });
 
 var app = builder.Build();
-
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
