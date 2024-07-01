21f7db80a2bc49075a76824e41bddaf3470be011
taleh.karimov <taleh.karimov@smartsolutions.az>
2024-07-01 15:07:59 +0400

    Add CRUD operations to discount service Configure sqlite database with ef

--- src/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -3,8 +3,8 @@
     public class Coupon
     {
         public int Id { get; set; }
-        public string ProductName { get; set; } = default;
-        public string Description { get; set; } = default;
+        public string ProductName { get; set; } = default!;
+        public string Description { get; set; } = default!;
         public int Amount { get; set; }
     }
 }
