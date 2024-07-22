804ad9779c0bbbf35946b4ddb46acd0eb32dffb1
taleh.karimov <taleh.karimov@smartsolutions.az>
2024-07-22 16:09:32 +0400

    Create order service

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _events = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _events.AsReadOnly();
+
+        public void AddDomainEvents(IDomainEvent domainEvent) => _events.Add(domainEvent);
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] dequeuedEvents = _events.ToArray();
+            _events.Clear();
+            return dequeuedEvents;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base($"Domain exception: \"{message}\" throws from Domain Layer") 
+        { }
+    }
+}
